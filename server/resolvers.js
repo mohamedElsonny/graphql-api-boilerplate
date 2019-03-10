56f23465619d0a83ab62b1a4592d7f79a01d8f24
mohamedElsonny <mohamedelsonny5@gmail.com>
2019-03-10 23:20:54 +0200

    adding signup user

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 module.exports = {
   Query: {
     user: (_, { id }, { prisma }, info) => {
@@ -23,6 +26,49 @@ module.exports = {
         },
         info
       )
+    },
+    signup: async (
+      _,
+      { data: { name, email, password } },
+      { prisma },
+      info
+    ) => {
+      try {
+        const existUser = await prisma.query.users({
+          where: {
+            email
+          }
+        })
+        if (existUser && existUser.length) {
+          return new Error('email already exists')
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const user = await prisma.mutation.createUser(
+          {
+            data: {
+              name,
+              email,
+              password: hash
+            }
+          },
+          `{
+          id
+          name
+          email
+        }`
+        )
+
+        const token = jwt.sign(
+          { id: user.id, email: user.email },
+          process.env.SECRET,
+          { expiresIn: '1h' }
+        )
+        return { token }
+      } catch (ex) {
+        console.error(ex)
+      }
     }
   }
 }
